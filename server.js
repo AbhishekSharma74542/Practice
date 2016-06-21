7bfbcd17b664d198aea26800ccdbddd6a584a0f0
Abhishek Sharma <abhishek.sharma74542@gmail.com>
2016-06-21 15:51:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 	//127.0.0.1/8 Local Host 172.31.57.218
 	console.log("We got Connected Let's Start");
 	//Defining Libs
-	var server = require('http').createServer();
-	var io = require('socket.io')(server);
-	io.on('connection', function(socket){
-		console.log("Connected");
-	  socket.on('event', function(data){
-	  	console.log("On Event ");
-	  	console.log(data);
-	  });
-	  socket.on('disconnect', function(){
-	  	console.log("On Disconnection ");
-	  });
+	var app = require('express')();
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+
+	app.get('/', function(req, res){
+	
+	  //send the index.html file for all requests
+	  res.sendFile(__dirname + '/index.html');
+	
 	});
-	server.listen(1330);
+	
+	http.listen(1330, function(){
+	
+	  console.log('listening on *:1330');
+	
+	});
+	
+	//for testing, we're just going to send data to the client every second
+	setInterval( function() {
+	
+	  /*
+	    our message we want to send to the client: in this case it's just a random
+	    number that we generate on the server
+	  */
+	  var msg = Math.random();
+	  io.emit('message', msg);
+	  console.log (msg);
+	
+	}, 1000);
 	//Defining Libs
 	var mysql      = require('mysql');
 	
