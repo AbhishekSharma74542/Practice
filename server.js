97168b1814b1b5d3402e1cfc4f43ddd51b3899fd
Abhishek Sharma <abhishek.sharma74542@gmail.com>
2016-06-21 16:49:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var net = require('net');
 
-app.listen(1330);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+var client = new net.Socket();
+client.connect(1330, '127.0.0.1', function() {
+	console.log('Connected');
+	client.write('Hello, server! Love, Client.');
 });
-console.log("app is listening to 1330 port");
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+	
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
+
