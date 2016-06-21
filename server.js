b7871045c2e5a84efee7238f4a2f7808085c74d9
Abhishek Sharma <abhishek.sharma74542@gmail.com>
2016-06-21 15:12:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 	console.log("We got Connected Let's Start");
 	//Defining Libs
-	var app = require('express')();
-	var server = require('http').createServer(app);
-	var io = require('socket.io')(server);
-	io.on('connection', function(){
-		console.log("Connection Setup Done !!!")
+	var io = require('socket.io')();
+	console.log("coming");
+	io.on('connection', function(socket){
+		console.log("Connected!!")
 	});
-	server.listen(1338);
+	console.log("coming 2");
+	io.listen(1338);
 	//Defining Libs
 	var mysql      = require('mysql');
 	
