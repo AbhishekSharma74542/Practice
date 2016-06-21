91cf6a2827853590ad0951af90ebad87ddb574f5
Abhishek Sharma <abhishek.sharma74542@gmail.com>
2016-06-21 14:47:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 	var mysql      = require('mysql');
 	
     //Defining Connection to MySQL
-	var connection = mysql.createConnection({
+	var myconnection = mysql.createConnection({
 	host     : 'cgdev2.cf53zztlzwqg.us-east-1.rds.amazonaws.com',
 	user     : 'cgdevuser',
 	password : 'cgdev123',
@@ -13,7 +13,7 @@
 	});
 
 	//Connecting MySQL 
-	connection.connect(function(err) {
+	myconnection.connect(function(err) {
 		  if (err) {
 		    console.error('error connecting: ' + err.stack);
 		    return;
@@ -24,20 +24,18 @@
 
 
 	io.on('connection', function(socket){
+	console.log("Connection Established");
 	socket.on('event', function(data){
+	console.log("Connection Established and on event");	
 	console.log("Data on Event is  :: "+data);
 
 
 	connection.connect();
 
-	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-	if (err) throw err;
-
-	console.log('The solution is: ', rows[0].solution);
-	});
 
 	});
 	socket.on('disconnect', function(){
+	console.log("Connection Terminated");
 	console.log("Data on Event is  :: "+data);
 	});
 	});
