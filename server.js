dc41d7f86947f3e9887b9e25129a4b536c8707d4
Abhishek Sharma <abhishek.sharma74542@gmail.com>
2016-06-21 16:36:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,26 @@
-	//127.0.0.1/8 Local Host 172.31.57.218
-	console.log("We got Connected Let's Start");
-	 var app = require('express')();
-	//Defining Libs
-	var server = require('http').createServer();
-	var io = require('socket.io')(server);
-	io.on('connection', function(socket){
-	  socket.on('data', function(data){
-	  	console.log("Event Occured");
-	  	console.log("Data"+data);
-	  	
-	  });
-	  socket.on('disconnect', function(){
-	  	console.log("Disconnected");
-	  	
-	  });
-	});
-	server.listen(1330);
-	var http = require('http').Server(app);
-	
-	app.get('/', function(req, res){
-	
-	  //send the index.html file for all requests
-	  res.sendFile(__dirname + '/index.html');
-	
-	});
-	
-	
-	//for testing, we're just going to send data to the client every second
-	setInterval( function() {
-	
-	  /*
-	    our message we want to send to the client: in this case it's just a random
-	    number that we generate on the server
-	  */
-	  var msg = Math.random();
-	  io.emit('message', msg);
-	  
-	
-	}, 5000);
-	//Defining Libs
-	var mysql      = require('mysql');
-	
-        //Defining Connection to MySQL
-	var myconnection = mysql.createConnection({
-	host     : 'cgdev2.cf53zztlzwqg.us-east-1.rds.amazonaws.com',
-	user     : 'cgdevuser',
-	password : 'cgdev123',
-	database : 'internDb'
-	});
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-	//Connecting MySQL 
-	myconnection.connect(function(err) {
-		  if (err) {
-		    console.error('error connecting: ' + err.stack);
-		    return;
-		  }
-		 
-		  console.log('connected as id ' + myconnection.threadId);
-	});
+app.listen(1330);
 
-  	
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+console.log("app is listening to 1330 port");
